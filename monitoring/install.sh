1fa914ca03154c537c063267dff6a6d887d581b8
gasparbarancelli <gasparbarancelli@gmail.com>
2021-05-27 15:51:03 -0300

    install grafana

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -7,3 +7,14 @@ kubectl apply -f "namespace.yaml"
 kubectl apply -f "operator.yaml"
 kubectl wait --for=condition=available --timeout=600s deployment/prometheus-operator -n monitoring
 kustomize build . | kubectl apply -f -
+
+
+curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.18.1/install.sh | bash -s v0.18.1
+
+kubectl create -f https://operatorhub.io/install/grafana-operator.yaml
+kubectl wait --for=condition=available --timeout=600s deployment/grafana-operator  -n my-grafana-operator
+
+kubectl apply -f grafana-volume.yaml
+kubectl apply -f grafana-datasource.yaml
+kubectl apply -f grafana-dashboard.yaml
+kubectl apply -f grafana.yaml
